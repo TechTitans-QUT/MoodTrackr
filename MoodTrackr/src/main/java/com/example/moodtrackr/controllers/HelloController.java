59bf301959412f84ef59f607a3c94a027b63a03f
shon1t <130427928+shon1t@users.noreply.github.com>
2024-04-27 22:23:10 +1000

    Replace some controllers

--- MoodTrackr/src/main/java/com/example/moodtrackr/controllers/HelloController.java
+++ MoodTrackr/src/main/java/com/example/moodtrackr/controllers/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.moodtrackr.controllers;
 
+import com.example.moodtrackr.HelloApplication;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button MoodInputButton;
 
+    // add other buttons for nav to other pages
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onMoodInputButtonClick() throws IOException {
+        Stage stage = (Stage) MoodInputButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mood-input-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
     }
 }
