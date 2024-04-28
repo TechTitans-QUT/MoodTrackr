6c710e4ec3351e334617ed903b0f1678c666ce60
shon1t <sdele4@outlook.com>
2024-04-29 00:30:05 +1000

    add some stuff

--- MoodTrackr/src/main/java/com/example/moodtrackr/MoodInputApplication.java
+++ MoodTrackr/src/main/java/com/example/moodtrackr/MoodInputApplication.java
@@ -1,8 +1,11 @@
 package com.example.moodtrackr;
 
+import com.example.moodtrackr.controllers.MoodInputController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,12 +16,13 @@ public class MoodInputApplication extends Application {
     public static final int HEIGHT = 100;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MoodInputApplication.class.getResource("mood-input-page.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MoodInputApplication.class.getResource("mood-input.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
