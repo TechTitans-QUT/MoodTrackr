253781a6e83f2bfa256edb7ae73d0874a88200ef
shon1t <sdele4@outlook.com>
2024-04-28 22:38:55 +1000

    add some studff

--- MoodTrackr/src/main/java/com/example/moodtrackr/controllers/MoodInput.java
+++ MoodTrackr/src/main/java/com/example/moodtrackr/controllers/MoodInput.java
@@ -4,11 +4,5 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class MoodInput {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("MoodInpu!");
-    }
 }
