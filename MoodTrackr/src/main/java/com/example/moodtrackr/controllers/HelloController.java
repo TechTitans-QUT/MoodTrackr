5b99d4f8fa557b885ec200c2c4c661601802d455
suzuki-02 <n10694218@qut.edu.au>
2024-04-29 15:45:51 +1000

    add login->Dashboard & logout

--- MoodTrackr/src/main/java/com/example/moodtrackr/controllers/HelloController.java
+++ MoodTrackr/src/main/java/com/example/moodtrackr/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.moodtrackr.controllers;
 
 import com.example.moodtrackr.HelloApplication;
+import com.example.moodtrackr.RegisterApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -12,6 +13,8 @@ import java.io.IOException;
 public class HelloController {
     @FXML
     private Button MoodInputButton;
+    @FXML
+    private Button logOutButton;
 
     // add other buttons for nav to other pages
     @FXML
@@ -19,6 +22,14 @@ public class HelloController {
         Stage stage = (Stage) MoodInputButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mood-input-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    protected void onLogOutButton() throws IOException {
+        Stage stage = (Stage) logOutButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), RegisterApplication.WIDTH, RegisterApplication.HEIGHT);
         stage.setScene(scene);
     }
 }
