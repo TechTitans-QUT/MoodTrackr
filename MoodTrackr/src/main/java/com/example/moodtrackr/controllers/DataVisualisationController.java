e3a5c6b2b2c68db8a81d5e641c7fa4d0cd2ffbe0
Joshua Kyle-Little <joshkylelittle@gmail.com>
2024-05-05 23:52:01 +1000

    Added base testing for templates

--- MoodTrackr/src/main/java/com/example/moodtrackr/controllers/DataVisualisationController.java
+++ MoodTrackr/src/main/java/com/example/moodtrackr/controllers/DataVisualisationController.java
@@ -2,8 +2,11 @@ package com.example.moodtrackr.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 
 public class DataVisualisationController {
+    public VBox sidebarTemplate;
+    public HBox bodyTemplate;
     @FXML
     private Label welcomeText;
 
