4d84671c4a52f13cd47449842991d6cf5a87e2c2
suzuki-02 <n10694218@qut.edu.au>
2024-05-12 21:30:42 +1000

    Combine applications into main application, trim unused controllers

--- MoodTrackr/src/main/java/com/example/moodtrackr/MainApplication.java
+++ MoodTrackr/src/main/java/com/example/moodtrackr/MainApplication.java
@@ -10,10 +10,16 @@ import java.io.IOException;
 public class MainApplication extends Application {
     public static final String TITLE = "MoodTrackr";
     public static final int WIDTH = 1075;
+//    public static final int width = 1000; // template-page
     public static final int HEIGHT = 680;
+//    public static final int height = 600; // template-page
+    public static final String login = "login-view.fxml";
+    public static final String template = "template-page.fxml";
+    public static final String dashboard = "hello-view.fxml";
+    public static final String moodInput = "mood-input-page.fxml";
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(login)); // Select launching page
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle(TITLE);
         stage.setScene(scene);
