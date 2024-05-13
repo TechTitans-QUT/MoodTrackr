8410eedfa4fa59c5ef3c82ac32a417af4235acb9
shon1t <sdele4@outlook.com>
2024-05-13 12:07:50 +1000

    commit random

---
+++ MoodTrackr/src/main/java/com/example/moodtrackr/controllers/TableViewController.java
@@ -0,0 +1,22 @@
+package com.example.moodtrackr.controllers;
+
+import com.example.moodtrackr.MoodInputApplication;
+import com.example.moodtrackr.model.Session;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class TableViewController {
+    @FXML
+    private TableView<Session> sessionTableView;
+
+
+}
