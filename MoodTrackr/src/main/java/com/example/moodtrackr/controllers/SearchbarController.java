2c3c206d68ab56eeaee1f2d011e3a59d9eea0e59
suzuki-02 <n10694218@qut.edu.au>
2024-05-20 01:40:10 +1000

    added hover effect on sidebar buttons

--- MoodTrackr/src/main/java/com/example/moodtrackr/controllers/SearchbarController.java
+++ MoodTrackr/src/main/java/com/example/moodtrackr/controllers/SearchbarController.java
@@ -2,13 +2,19 @@ package com.example.moodtrackr.controllers;
 
 import com.example.moodtrackr.GlobalData;
 import com.example.moodtrackr.model.User;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.TransferQueue;
 
 public class SearchbarController implements Initializable {
     @FXML
